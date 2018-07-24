6b106a1087bb06feed1f77c2f106117ab29c7fad
mvogt <moritz.vogt@vogges.de>
2018-07-24 20:30:01 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+// Set Engine
+app.set('view engine', 'hbs');
+
+
+
+//Register MiddleWARE
+app.use((req, res, next) => {
+    //req Everthing that comes from the client
+
+    var now  = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+
+    console.log(log);
+    //write to filesystem
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// Register Middleware
+//app.use((req, res, next) => {
+//    res.render('maintance.hbs')
+//});
+
+//Register MiddleWARE
+app.use(express.static(__dirname + '/public'));
+
+//Helper for Templating
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+//Routing
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express</h1>f');
+
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to my page'
+    });});
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error du schmock'
+    })
+})
+app.listen(3000, () => {
+    console.log('Server is up on Port 3000');
+});
